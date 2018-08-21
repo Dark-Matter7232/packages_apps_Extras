66a9389bd835b28bb2b442f4bb14099d183d909f
Simrat Singh <simrats169169@gmail.com>
2018-08-21 16:59:16 +0530

    Return the correct Metrics event

--- src/com/extras/settings/preferences/ActionFragment.java
+++ src/com/extras/settings/preferences/ActionFragment.java
@@ -256,6 +256,6 @@ public class ActionFragment extends SettingsPreferenceFragment implements
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.NITROGEN_SETTINGS;
+        return MetricsProto.MetricsEvent.EXTRAS;
     }
 }
