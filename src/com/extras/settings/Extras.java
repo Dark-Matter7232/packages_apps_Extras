a58b4b41823b8a8578f3fa2c831f2ad130570095
simrat39 <simrats169169@gmail.com>
2018-09-20 19:20:14 +0530

    Extras.java : Cleanup

--- src/com/extras/settings/Extras.java
+++ src/com/extras/settings/Extras.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2016 The Pure Nexus Project
- * used for Nitrogen OS
+ * Copyright (C)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,40 +19,22 @@ package com.extras.settings;
 import com.android.internal.logging.nano.MetricsProto;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Surface;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
+import android.preference.Preference;
 import com.android.settings.R;
-import com.extras.settings.preferences.Utils;
 
 import com.android.settings.SettingsPreferenceFragment;
 
-public class Extras extends SettingsPreferenceFragment {
-
-    private static final String RECENTS_CATEGORY = "recents_category";
-    private static final String ACTION_QUICKSTEP = "android.intent.action.QUICKSTEP_SERVICE";
+public class CustomSettings extends SettingsPreferenceFragment {
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.extras_settings);
-
-        PreferenceScreen prefSet = getPreferenceScreen();
-        ContentResolver resolver = getActivity().getContentResolver();
-        Context context = getActivity().getApplicationContext();
-
-        if (isNewRecents(context)) {
-            prefSet.removePreference(findPreference(RECENTS_CATEGORY));
-        }
     }
 
     @Override
@@ -88,18 +69,5 @@ public class Extras extends SettingsPreferenceFragment {
                 break;
         }
         activity.setRequestedOrientation(frozenRotation);
-    }
-
-    boolean isNewRecents(Context context) {
-
-        final ComponentName recentsComponentName = ComponentName.unflattenFromString(
-                context.getString(com.android.internal.R.string.config_recentsComponentName));
-        final Intent quickStepIntent = new Intent(ACTION_QUICKSTEP)
-                .setPackage(recentsComponentName.getPackageName());
-        if (context.getPackageManager().resolveService(quickStepIntent,
-                PackageManager.MATCH_SYSTEM_ONLY) == null) {
-            return false;
-        }
-        return true;
     }
 }
